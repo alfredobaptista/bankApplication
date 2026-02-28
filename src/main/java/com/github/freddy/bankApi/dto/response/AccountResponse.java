13fc2dcc45da6878647733f3f6a8c5c42898d293
Alfredo <baptistaalfredo81@gmail.com>
2026-02-28 07:37:10 +0100

    refator: chance name initialNalnace to balance

--- src/main/java/com/github/freddy/bankApi/dto/response/AccountResponse.java
+++ src/main/java/com/github/freddy/bankApi/dto/response/AccountResponse.java
@@ -7,7 +7,7 @@ import java.math.BigDecimal;
 
 public record AccountResponse(
         String accountNumber,
-        BigDecimal initialBalance,
+        BigDecimal balance,
         String currencyCode,
         AccountType accountType,
         AccountStatus accountStatus
